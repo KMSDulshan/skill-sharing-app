de0f53835dcca103125ca0be3657e7cd281773cb
K.M.S Dulshan <152279802+KMSDulshan@users.noreply.github.com>
2025-04-28 08:17:34 +0530

    feet:frontend pages create

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        {/* Add more routes here */}
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
